39dfaa837d26e37eeb2134364a9c9e4ec99b36bd
Simul-Eqn <89591030+Simul-Eqn@users.noreply.github.com>
2025-03-18 18:23:27 +0800

    make colour test

--- tests/test_colour.py
+++ tests/test_colour.py
@@ -2,16 +2,31 @@
 import sys 
 sys.path.insert(1, "./../src")
 
+from pathlib import Path
+savedir = Path(__file__).parent 
+
 import turtlethread 
-from turtlethread import stitches 
 
 te = turtlethread.Turtle() 
-with te.running_stitch(9999): 
-    te.forward(100) 
 
-for colour in ['green', 'blue', 'black', 'blue']: 
-    te.color(colour) 
+for color in ['green', 'blue', 'black', 'blue']: 
+    te.color(color) 
     with te.running_stitch(9999): 
         te.forward(100) 
+    te.right(90) 
+
+te.right(195) 
+
+for color in ['red', 'green', 'red']: 
+    te.color(color) 
+    with te.running_stitch(9999): 
+        te.forward(150) 
+        te.right(120) 
+
 
 print(te.pattern.to_pyembroidery().threadlist) 
+
+
+
+te.save(str(savedir / 'test_colour.exp'), str(savedir / 'test_colour.inf')) 
+te.save(str(savedir / 'test_colour.png'))
