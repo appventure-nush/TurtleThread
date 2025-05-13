42cfe6c343ae7d1af079145b50f515e76634668d
Walnit <39372439+Walnit@users.noreply.github.com>
2025-05-13 20:29:39 +0800

    Fix text test for workflow

--- tests/minimal/test_text_outlines_and_full_fill.py
+++ tests/minimal/test_text_outlines_and_full_fill.py
@@ -6,30 +6,33 @@ from pytest import approx
 
 from turtlethread import Turtle, LetterDrawer
 
+import os
+font_name = "dejavuserif" if "CI" in os.environ else "Arial" # GitHub CI does not have Arial font
+
 @pytest.fixture
 def turtle():
     return Turtle(angle_mode="degrees")
 
 def test_outline_text(turtle):
     with LetterDrawer(turtle) as ld:
-        ld.load_font('Arial')
-        ld.draw_one_letter('Arial', 'T', 120, fill=False, outline=True) 
+        ld.load_font(font_name)
+        ld.draw_one_letter(font_name, 'T', 120, fill=False, outline=True) 
         ld.draw_letter_gap(120)
-        ld.draw_string('Arial', 'est', 120, fills=False, outlines=True)
+        ld.draw_string(font_name, 'est', 120, fills=False, outlines=True)
     turtle.visualise(skip=True, done=False, bye=False)
 
 def test_full_fill_text(turtle):
     with LetterDrawer(turtle) as ld:
-        ld.load_font('Arial')
-        ld.draw_one_letter('Arial', 'T', 120, fill=True, outline=False) 
+        ld.load_font(font_name)
+        ld.draw_one_letter(font_name, 'T', 120, fill=True, outline=False) 
         ld.draw_letter_gap(120)
-        ld.draw_string('Arial', 'est', 120, fills=True, outlines=False)
+        ld.draw_string(font_name, 'est', 120, fills=True, outlines=False)
     turtle.visualise(skip=True, done=False, bye=False)
 
 def test_both_outline_and_full_fill_text(turtle):
     with LetterDrawer(turtle) as ld:
-        ld.load_font('Arial')
-        ld.draw_one_letter('Arial', 'T', 120, fill=True, outline=True) 
+        ld.load_font(font_name)
+        ld.draw_one_letter(font_name, 'T', 120, fill=True, outline=True) 
         ld.draw_letter_gap(120)
-        ld.draw_string('Arial', 'est', 120, fills=True, outlines=True)
+        ld.draw_string(font_name, 'est', 120, fills=True, outlines=True)
     turtle.visualise(skip=True, done=False, bye=False)
